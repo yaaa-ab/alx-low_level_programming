1c1c0427815de544f0af1bc46313978e7cd85fe0
Yared <yatsegay@gmail.com>
2022-10-21 16:57:43 +0300

    fix on main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-void print_number(int n);
-void print_triangle(int size);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-int main(void);
-int _putchar(char c);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
